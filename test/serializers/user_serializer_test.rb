d17cc45df577620d05d4131693be37bfa0039b86
chriskottom <chris@chriskottom.com>
2017-03-22 12:33:37 +0100

    Test the :links included by UserSerializer
    Write a test that demonstrates that the links included in the
    serialized representation of a User model are the correct ones -
    one for the User itself and another for the Boards created by that
    User.
    
    Also, extract the logic for creating the serialized representation
    of the user to a private helper method.

--- test/serializers/user_serializer_test.rb
+++ test/serializers/user_serializer_test.rb
@@ -1,16 +1,35 @@
 require 'test_helper'
 
 class UserSerializerTest < ActiveSupport::TestCase
+  include Rails.application.routes.url_helpers
+
   setup do
     @user = users(:admin)
   end
 
   test 'serializes all visible attributes for a User' do
-    serializer = UserSerializer.new(@user)
-    serialized_user = serializer.as_json
-    
     %i(id email gravatar_url admin created_at updated_at).each do |attribute|
       assert_equal @user.send(attribute), serialized_user[attribute]
     end
+  end
+
+  test 'includes links for the User' do
+    serialized_links = serialized_user[:links]
+    assert_equal 2, serialized_links.count
+
+    self_url = serialized_links.find { |url| url[:rel] == :self }
+    refute_nil self_url
+    assert_equal user_url(@user), self_url[:href]
+
+    boards_url = serialized_links.find { |url| url[:rel] == :boards }
+    refute_nil boards_url
+    assert_equal user_boards_url(@user), boards_url[:href]
+  end
+
+  private
+
+  def serialized_user(user = nil)
+    user ||= @user
+    UserSerializer.new(user).as_json
   end
 end
