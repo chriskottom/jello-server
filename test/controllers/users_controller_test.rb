d2e128c9ca9301ee7a30be8ada2799fb07f86496
chriskottom <chris@chriskottom.com>
2017-03-23 09:22:32 +0100

    Add happy path test for UsersController#show
    This test demonstrates that the response to the request is delivered
    as expected including the format and content of the response body.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 describe UsersController do
   before do
+    @user = users(:admin)
   end
 
   describe 'GET /users' do
@@ -22,6 +23,29 @@ describe UsersController do
   end
 
   describe 'GET /users/:id' do
+    it 'should successfully fetch the requested User' do
+      get user_url(@user)
+      assert_response :ok
+
+      response_user = json_response['user']
+
+      expected_keys = %w(id email gravatar_url admin created_at updated_at
+                         links active_boards)
+      assert_keys expected_keys, response_user
+
+      assert_equal @user.id, response_user['id']
+      assert_equal @user.email, response_user['email']
+      assert_equal @user.gravatar_url, response_user['gravatar_url']
+
+      self_link = { 'rel' => 'self', 'href' => user_url(@user) }
+      assert_link self_link, response_user, 'links'
+
+      boards_link = { 'rel' => 'boards', 'href' => user_boards_url(@user) }
+      assert_link boards_link, response_user, 'links'
+
+      board_ids = @user.boards.where(archived: [false, nil]).pluck(:id)
+      assert_ids board_ids, response_user['active_boards'], 'id'
+    end
   end
 
   describe 'POST /users' do
