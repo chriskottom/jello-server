6a35cd77fa47db2f2036c2bb087d41b00c18d94c
chriskottom <chris@chriskottom.com>
2017-03-23 08:34:33 +0100

    Move to Minitest::Spec for controller testing

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,43 +1,21 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = users(:admin)
-    @user_attributes = {
-      email: 'joe@example.com',
-      password: 'secret',
-      password_confirmation: 'secret'
-    }
+describe UsersController do
+  before do
   end
 
-  test "should get index" do
-    get users_url, as: :json
-    assert_response :success
+  describe 'GET /users' do
   end
 
-  test "should create user" do
-    assert_difference('User.count') do
-      post users_url, params: { user: @user_attributes }, as: :json
-    end
-
-    assert_response 201
+  describe 'GET /users/:id' do
   end
 
-  test "should show user" do
-    get user_url(@user), as: :json
-    assert_response :success
+  describe 'POST /users' do
   end
 
-  test "should update user" do
-    patch user_url(@user), params: { user: @user_attributes }, as: :json
-    assert_response 200
+  describe 'PATCH /users/:id' do
   end
 
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete user_url(@user), as: :json
-    end
-
-    assert_response 204
+  describe 'DELETE /users/:id' do
   end
 end
