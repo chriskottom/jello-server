4a80477ed19127fce518792282fedf5eaa687ad8
chriskottom <chris@chriskottom.com>
2017-03-23 14:57:49 +0100

    Establish API v1 using a routing scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   subdomain = ENV.fetch('API_SUBDOMAIN', '')
   constraints subdomain: subdomain do
-    resources :users do
-      resources :boards, only: [:index]
+    scope :v1 do
+      resources :users do
+        resources :boards, only: [:index]
+      end
+      resources :boards
+      resources :lists
+      resources :cards
+      resources :comments
     end
-    resources :boards
-    resources :lists
-    resources :cards
-    resources :comments
   end
 end
