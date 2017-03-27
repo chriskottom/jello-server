f54eff115299733db088253ae15cbaadd6a16d91
chriskottom <chris@chriskottom.com>
2017-03-27 09:25:34 +0200

    Standardize responses for rate-limited requests
    Requests that are throttled should receive an error response that
    follows previously established standards including:
    
    - Proper use of X-RateLimit headers
    - JSON response body

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,3 +2,29 @@
 Rack::Attack.throttle("req/ip", :limit => 10, :period => 1.second) do |req|
   req.ip
 end
+
+# Customize the response delivered to rate limited clients.
+Rack::Attack.throttled_response = ->(env) {
+  status_code = 429
+  now = Time.now
+  match_data = env['rack.attack.match_data'] || {}
+  retry_after = match_data[:period] - now.to_i % match_data[:period]
+
+  headers = {
+    'Content-Type' => 'application/json',
+    'Retry-After' => retry_after.to_s,
+    'X-RateLimit-Limit' => match_data[:limit].to_s,
+    'X-RateLimit-Remaining' => '0',
+    'X-RateLimit-Reset' => (now + retry_after).to_s
+  }
+
+  body = {
+    error: {
+      status: status_code,
+      name: "Too Many Requests",
+      message: "Throttle limit reached. Retry later."
+    }
+  }
+
+  [ status_code, headers, [body.to_json] ]
+}
