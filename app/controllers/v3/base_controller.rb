54a8921dade0b5f60c536d46f95f0a455a3059e5
chriskottom <chris@chriskottom.com>
2017-03-30 15:57:30 +0200

    Customize 401 Unauthorized response
    To format error responses from Doorkeeper, we must override the
    doorkeeper_unauthorized_render_options for all API controllers.

--- app/controllers/v3/base_controller.rb
+++ app/controllers/v3/base_controller.rb
@@ -1,3 +1,21 @@
 class V3::BaseController < ApplicationController
   before_action :doorkeeper_authorize!
+
+  def doorkeeper_unauthorized_render_options(error: nil)
+    message = 'Not authorized to perform requested action'
+    if error && error.respond_to?(:description)
+      message = error.description
+    end
+
+    {
+      status: :unauthorized,
+      json: {
+        error: {
+          status: 401,
+          name: "Unauthorized",
+          message: message
+        }
+      }
+    }
+  end
 end
