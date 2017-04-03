4654693d7a55f0a8d4b7b8dff2c8ff58e6790cda
chriskottom <chris@chriskottom.com>
2017-04-03 17:16:06 +0200

    Refactor error rendering helpers into a separate concern

---
+++ app/controllers/concerns/responds_with_error.rb
@@ -0,0 +1,26 @@
+module RespondsWithError
+  private
+
+  def render_error(status, resource = nil)
+    render status: status,
+           json: (resource ? { error: resource } : nil)
+  end
+
+  def respond_with_not_found(exception)
+    render_error :not_found,
+                 {
+                   status: 404,
+                   name: "Not found",
+                   message: exception.message
+                 }
+  end
+
+  def respond_with_validation_error(model, errors = nil)
+    render_error :unprocessable_entity,
+                 {
+                   status: 422,
+                   name: "Validation failed",
+                   errors: (errors || model.errors)
+                 }
+  end
+end
